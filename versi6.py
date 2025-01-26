3ae847582afa7df8e226a430d10d7ff339f516fb
Rega Arzula Akbar <108121767+regaarz@users.noreply.github.com>
2025-01-26 20:53:13 +0700

    Update versi6.py

--- versi6.py
+++ versi6.py
@@ -196,7 +196,7 @@ def main():
 
         #Tulisan singkatan
         header_image2 = Image.open('Serasi4.png')  
-        col1, col2, col3 = st.columns([2.6, 4, 1])  
+        col1, col2, col3 = st.columns([2.6, 2, 1])  
         with col2:
             st.image(header_image2, width=200)
 
